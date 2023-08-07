73498d95d6823acad34351f226d22420558f08e8
Ivan Duarte <ivan.duarte@riverkidstexas.com>
2023-08-07 12:35:25 -0700

    removing dummy gen

--- src/index.ts
+++ src/index.ts
@@ -12,30 +12,10 @@ export default class GildedRose {
   items: Item[] = [];
 
   constructor(newItems: Item[] = []) {
-    if(newItems.length === 0) {
-      newItems = this.generateDummyItems();
-    }
-
     this.items = newItems;
   }
 
-  generateDummyItems(): Item[] {
-    const dummyItems: Item[] = [];
-
-    dummyItems.push(new Item('+5 Dexterity Vest', 10, 20));
-    dummyItems.push(new Item('Aged Brie', 2, 0));
-    dummyItems.push(new Item('Elixir of the Mongoose', 5, 7));
-    dummyItems.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-    dummyItems.push(
-      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)
-    );
-    dummyItems.push(new Item('Conjured Mana Cake', 3, 6));
-
-    return dummyItems;
-  }
-
   updateQuality(): Item[] {
-    
     const itemsUpdatedQuality: Item[] = this.items.map((item: Item) => {
       const itemName = item.name.toUpperCase();
       if (itemName.includes(ITEM_TYPES.SULFURAS)) {
